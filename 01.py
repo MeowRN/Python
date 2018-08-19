26c03faa1bb59e7111fb9275be24e221d553c495
Meow <vincentorium@outlook.com>
2018-08-19 18:55:58 +0800

    test

---
+++ 01.py
@@ -0,0 +1,55 @@
+
+#一個學生類，用來形容學
+
+# 定義一個空的類
+class Student():
+    # 一個孔磊，pass代表直接跳過
+    # 此處必須由pass
+    pass
+
+# 定義一個對象
+
+mingyue = Student()
+
+
+# 在定義一個類，用來描述聽python的學生
+
+class PythonStudent():
+      # 用NONE給不確定的值賦值
+      name = None
+      age = 18
+      course = "python"
+
+      def dohomework(self):
+          print("I 在做作業")
+
+          return None
+
+# 實例化一個名爲yueyue的學生
+
+
+yueyue = PythonStudent()
+
+print(yueyue.name)
+print(yueyue.age)
+
+yueyue.name
+yueyue.age
+# 注意成員函數的調用沒有傳遞進入參數
+yueyue.dohomework()
+
+
+
+
+
+#VS
+
+#fun class():
+    #name
+
+#Note:
+
+#函數爲
+
+#class+類（大駝峯）+（）+：
+
